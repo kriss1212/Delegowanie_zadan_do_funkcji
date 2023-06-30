4ed549b9e241c7026c27bc919f583c98cbb60126
kriss1212 <krzysiekzory1@o2.pl>
2023-06-30 21:14:43 +0200

    add ew files

---
+++ Dwa zbliżone/Dwa zbliżone/Program.cs
@@ -0,0 +1,58 @@
+﻿
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        int[,] tablica = {
+{1, 2, 3},
+{4, 5, 6},
+{7, 8, 9},
+{10, 11, 12}
+};
+
+        int[] najblizszeWiersze = ZnajdzNajblizszeWiersze(tablica);
+        Console.WriteLine($"Najbardziej zbliżone wiersze: {najblizszeWiersze[0]}, {najblizszeWiersze[1]}");
+        Console.ReadLine();
+    }
+
+    static int[] ZnajdzNajblizszeWiersze(int[,] tablica)
+    {
+        int iloscWierszy = tablica.GetLength(0);
+        int minRoznica = int.MaxValue;
+        int[] najblizszeWiersze = new int[2];
+
+        for (int i = 0; i < iloscWierszy - 1; i++)
+        {
+            for (int j = i + 1; j < iloscWierszy; j++)
+            {
+                int roznica = ObliczRozniceWierszy(tablica, i, j);
+                if (roznica < minRoznica)
+                {
+                    minRoznica = roznica;
+                    najblizszeWiersze[0] = i;
+                    najblizszeWiersze[1] = j;
+                }
+            }
+        }
+
+        return najblizszeWiersze;
+    }
+
+    static int ObliczRozniceWierszy(int[,] tablica, int wiersz1, int wiersz2)
+    {
+        int iloscKolumn = tablica.GetLength(1);
+        int roznica = 0;
+
+        for (int k = 0; k < iloscKolumn; k++)
+        {
+            int aik1 = tablica[wiersz1, k];
+            int aik2 = tablica[wiersz2, k];
+            roznica += (aik1 - aik2) * (aik1 - aik2);
+        }
+
+        return roznica;
+    }
+}
+
---
+++ Uklład kartezjański/Uklład kartezjański/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+public class Program
+{
+    public static void Main()
+    {
+        double x1 = 2.0; // współrzędna x początkowego punktu
+        double y1 = 3.0; // współrzędna y początkowego punktu
+        double x2 = 5.0; // współrzędna x końcowego punktu
+        double y2 = 7.0; // współrzędna y końcowego punktu
+
+        double dlugosc = ObliczDlugoscOdcinka(x1, y1, x2, y2);
+        Console.WriteLine("Długość odcinka: " + dlugosc);
+    }
+
+    public static double ObliczDlugoscOdcinka(double x1, double y1, double x2, double y2)
+    {
+        double dx = x2 - x1;
+        double dy = y2 - y1;
+        double dlugosc = Math.Sqrt(dx * dx + dy * dy);
+        return dlugosc;
+    }
+}
